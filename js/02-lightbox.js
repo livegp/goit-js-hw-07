28973be059fa7a65d769da6b878277ee0c6f2f16
Oleksandr <live.gp@gmail.com>
2023-04-30 15:20:35 +0300

    simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}" 
+                        alt="${description}" />
+                    </a>
+                </li>
+                `;
+        })
+        .join('');
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        enableKeyboard: true,
+        showCounter: false,
+        scrollZoom: false,
+        close: false,
+        onShow: () => {
+            document.addEventListener('keydown', onEscPress);
+        }
+    }); 
+}
 
 console.log(galleryItems);
