d2dc86f8e3840bf5bfe12135dc1cbc70fa7c03d8
Oleksandr <live.gp@gmail.com>
2023-04-30 14:21:53 +0300

    basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+    }).join('');
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+    gallery.addEventListener('keydown', onEscPress);
+    function onEscPress(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    }
+}
 
 console.log(galleryItems);
