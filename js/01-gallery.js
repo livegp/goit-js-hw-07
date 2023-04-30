c06900452c7d26b2577d37846db8949aba1be2a1
Oleksandr <live.gp@gmail.com>
2023-04-30 15:21:04 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,19 +7,20 @@ gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 gallery.addEventListener('click', onGalleryClick);
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `
-        <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-                />
-            </a>
-        </li>`;
-    }).join('');
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img 
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"/>
+                    </a>
+                </li>`;
+        })
+        .join('');
 }
 
 function onGalleryClick(event) {
